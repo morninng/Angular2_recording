aefb48e1d65c2c9510e1b820fe8b57a8557da9b6
morninng <mr_morninng_star_dash@yahoo.co.jp>
2016-09-03 17:51:01 +0900

    ngZone.run can update the view with not registered callback such as getusermedia

--- src/app/sample_test/worker/worker.component.ts
+++ src/app/sample_test/worker/worker.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit, provide } from '@angular/core';
+import { Component, OnInit, provide, ChangeDetectorRef, ChangeDetectionStrategy, NgZone } from '@angular/core';
 import {RecordMp3Service} from './record-mp3.service';
 import {NgClass} from '@angular/common';
+import { Subject } from 'rxjs/Rx';
 
 //import {Window} from './window';
 //import {Navigator} from './navigator';
@@ -18,7 +19,8 @@ declare var navigator:any;
   templateUrl: './worker.component.html',
   styleUrls: ['./worker.component.css'],
   providers: [RecordMp3Service],
-  directives: [NgClass]
+  directives: [NgClass],
+  changeDetection: ChangeDetectionStrategy.Default
 })
 export class WorkerComponent implements OnInit {
 
@@ -27,8 +29,27 @@ export class WorkerComponent implements OnInit {
   recording : boolean;
   audio_exist : boolean = false;
   audio_src : string;
+  audio_enabled : boolean;
+  text_data : string = "111";
+  text_data2 : string = "sss";
+  audio_context: any;
 
-  constructor( private record_mp3: RecordMp3Service) {
+  constructor( private record_mp3: RecordMp3Service, private ref: ChangeDetectorRef, private _ngZone: NgZone) {
+
+    this.text_data = "222";
+
+    const that = this;
+
+
+  }
+
+
+
+
+
+  ngOnInit() {
+
+
 
       window.AudioContext = window.AudioContext || window.webkitAudioContext;
       navigator.getUserMedia = ( navigator.getUserMedia ||
@@ -37,21 +58,17 @@ export class WorkerComponent implements OnInit {
                        navigator.msGetUserMedia);
       window.URL = window.URL || window.webkitURL;
 
-      const audio_context = new window.AudioContext();
-
+      this.audio_context = new window.AudioContext();
       navigator.getUserMedia({audio: true},
-        (stream)=>{
-          console.log("stream start");
-          const input = audio_context.createMediaStreamSource(stream);
-          record_mp3.update_setting(input, {numChannels: 1})
-        },
+        this.callback_getusermedia.bind(this),
         (e)=>{
           console.log('No live audio input: ' + e);
+          alert("you cannot use this system without activating the audio.")
         }
       );
 
 // http://www.gcgate.jp/engineerblog/2014/03/28/874/
-      record_mp3.audio_source$.subscribe(
+      this.record_mp3.audio_source$.subscribe(
         (audio_blob)=>{
           const audio_src = window.URL.createObjectURL(audio_blob);
           this.audio_src = audio_src
@@ -59,16 +76,40 @@ export class WorkerComponent implements OnInit {
           console.log("audio recorded");
         }
       )
-  }
 
-
-
-  ngOnInit() {
 
     this.recordable = true;
     this.recording = false;
 
   }
+
+  callback_getusermedia = (stream) => {
+      console.log("stream start");
+      const input = this.audio_context.createMediaStreamSource(stream);
+      this.record_mp3.update_setting(input, {numChannels: 1}) 
+
+      this._ngZone.run(()=>{
+        console.log(this);
+        this.audio_enabled = true;
+        this.text_data = "444";
+      })
+  
+      //this.ref.markForCheck();
+
+  }
+
+
+
+  update_data = ()=>{
+    this.text_data2 = "777";
+  }
+
+/*
+  test_test(){
+    this.that.text_data = "666"
+  }
+  */
+
 
   startRecording = ()=>{
     console.log("start recording");
